cf60bf30ac84fa3bc9dcf741aded0c733090e01b
jeroendv <fly.jdv@gmail.com>
2013-04-02 15:33:10 +0200

    Extend Maze documentation

--- src/penomazefiles/Maze.py
+++ src/penomazefiles/Maze.py
@@ -9,12 +9,28 @@ Module for code related to maze files
 class Maze(object):
     """A Maze is a collection of 'Tile' objects
 
+    Each tile has a unique coordinate assciated with it and together they form
+    a maze.
 
-    Each tile has a unique integer coordinate assciated with it and
-    together they form a maze.
+    The coordinates (x,y) are integers, where y increases downward and x
+    increases from left ot right as illustrated below
 
-    Tiles have a dimension of 1 by 1 and the its coordinate denotes 
-    its left upper corner.
+      0       1      2      3      4   x
+       +------+------+------+------+----> 
+       |(0,0)  (1,0)  (2,0)  (3,0)
+       | 
+      1+      +      +      +      +
+       |(0,1)  (1,1)  (2,1)  (3,1)
+       |
+      2+      +      +      +      +
+       |(0,2)  (1,2)  (2,2)  (3,2)
+      y|
+       V
+
+
+    Tiles have a dimension of 1 by 1 and its associated coordinate denotes the
+    left upper corner of the tile.
+
 
     todo: type checking of the methods
     todo: robustify for None arguments
@@ -24,8 +40,12 @@ class Maze(object):
     """
     The maze is stored as a dictionary of Tile objects with the tile
     coordinates a its key.
+            { (x,y) -> Tile } 
+    This ensures that each coordinate can only be
+    associated with one tile.
+
     """
-    _maze = {};
+    _maze = {}
 
     def __init__(self):
         super(Maze, self).__init__()
@@ -38,5 +58,25 @@ class Maze(object):
         If a tile is already present on that coordinate then it gets replaced.
         """
         self._maze[coordinate] = tile
+
+    def get_tile(self, coordinate):
+        """
+        Return the tile at a given coordinate.
+
+        Return None if there is no tile at the given coordinate
+        """
+        #todo: implement
+        raise NotImplementedError()
+
+    def get_rectangular_bbox(self):
+        """
+        Compute a bounding box of the current maze.
+
+        Returns  a tuple of coordinates (lu, rl). lu is the coordinate of the
+        left upper point of the bounding box while rl is the coordinate of the
+        right lower point of the bounding box
+        """
+        #todo: implement
+        raise NotImplementedError()
 
 
