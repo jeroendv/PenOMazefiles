210908380d3655acacded2b082cbe0a8119bb220
jeroendv <fly.jdv@gmail.com>
2013-04-03 14:16:27 +0200

    Implement MazeFileParser and add some test cases

--- src/penomazefiles/test_mazefileparser.py
+++ src/penomazefiles/test_mazefileparser.py
@@ -42,5 +42,67 @@ class Test_MazeFileTokenizer(unittest.TestCase):
 
 
 
+class Test_MazeFileParser(unittest.TestCase):
+    """Test of the MazeFileParser class"""
+
+    def setUp(self):
+        # string list corresponding the lines of apossible mazefile
+        self.input_tokenlist= ['2','3',
+                  'token.1', 'token.2',
+                  'token.3', 'token.4',
+                  'token.5', 'token.6']
+
+        # the list of tokens in present in the linestream
+        self.true_outputlist = [
+                  ((0,0),'token.1'), ((1,0),'token.2'),
+                  ((0,1),'token.3'), ((1,1),'token.4'),
+                  ((0,2),'token.5'), ((1,2),'token.6')]
+
+    def test_valid_tokenstream(self):
+        """parse a valid 2x3 mazefile token stream"""
+        
+        # create MazeFileParser and subscribe a consumer to build list of
+        # produced tokens which can then be compared with the true output list
+        parser = MazeFileParser()
+        outputlist = []
+        parser.add_token_parser(outputlist.append)
+
+        for token in self.input_tokenlist:
+            parser.consumeToken(token)
+
+        self.assertEqual(outputlist, self.true_outputlist)
+
+    @unittest.expectedFailure
+    def test_incomplete_tokenstream(self):
+        """
+        test an incomplete 2x3 mazefile token stream
+
+        fixme: introduce a start and end token to identify the start of
+        mazefile token stream and the end of one. This is the only way to
+        detect that tiles are missing. instead of still on the way.
+        """
+        
+        del self.input_tokenlist[-1]
+
+        parser = MazeFileParser()
+        with self.assertRaises(SpecificationViolationError):
+            for  token in self.input_tokenlist:
+                parser.consumeToken(token)
+            
+    def test_tobig_tokenstream(self):
+        """
+        test an 2x3 mazefile token stream which contains one tile token to much
+        """
+        
+        self.input_tokenlist.append(((10,10),'token'))
+
+        parser = MazeFileParser()
+        with self.assertRaises(SpecificationViolationError):
+            for  token in self.input_tokenlist:
+                parser.consumeToken(token)
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
