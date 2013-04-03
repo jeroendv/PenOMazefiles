661b2d53b081e366c48380762be253dbd6652924
jeroendv <fly.jdv@gmail.com>
2013-04-03 10:40:19 +0200

    Remove the TokenListBuilder object
    There is no need for TokenListBuilderObject, simply use a list object and
    register the list.append method as the token consumer

--- src/penomazefiles/test_mazefileparser.py
+++ src/penomazefiles/test_mazefileparser.py
@@ -30,27 +30,18 @@ class Test_MazeFileTokenizer(unittest.TestCase):
 
         #initialize TokenListBuilder object and register it's consumer method
         #with the tokenizer
-        token_listbuilder = TokenListBuilder()
-        t.addTokenConsumer(token_listbuilder.consume)
+        tokenlist = []
+        t.addTokenConsumer(tokenlist.append)
 
 
         # start the tokenizing process
         t.start()
 
 
-        self.assertEqual(token_listbuilder.tokenlist,true_tokenlist)
+        self.assertEqual(tokenlist,true_tokenlist)
 
 
 
-class TokenListBuilder(penomazefiles.mazefileparser.TokenConsumer):
-    """
-    Token consumer that builds a list of all consumed tokens produced by the
-    MazeFileTokenizer
-    """
-    tokenlist = []
-
-    def consume(self, token):
-        self.tokenlist.append(token)
 
 if __name__ == '__main__':
     unittest.main()
